33b18409046dfe153481cdc4dab62f7156a79436
Captain Cobbs <egamezhq@gmail.com>
2021-08-19 18:11:04 -0600

    Added overlay text

--- HexRPG/MainGame.cs
+++ HexRPG/MainGame.cs
@@ -3,11 +3,14 @@ using HexRPG.Entity;
 using HexRPG.Generation;
 using HexRPG.Managers;
 using HexRPG.Sprite;
+using HexRPG.UI;
+using HexRPG.Fonts;
 using HexRPG.Utilities;
 using HexRPG.World;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using static HexRPG.Utilities.FileUtilities;
 
 namespace HexRPG
@@ -31,6 +34,11 @@ namespace HexRPG
         SpriteBatch spriteBatch;
         MapManager mapViewer;
 
+        // Art
+        SpriteFont[] fonts;
+
+        // Overlay
+        private FrameCounter _frameCounter = new FrameCounter();
 
         /// <summary>
         /// Main Instance of the game that instantiates and creates all primary properties, generators, and managers.
@@ -56,12 +64,18 @@ namespace HexRPG
         protected override void Initialize()
         {
             base.Initialize();
+            InputManager.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             TextureIndex.LoadContent(Content);
+            fonts = new SpriteFont[]
+            {
+                FontLibrary.Battlenet(GraphicsDevice)
+            };
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -72,17 +86,31 @@ namespace HexRPG
 
 
             // Main Updates
-            InputManager.Update(IsActive, this);
+            Action toggleFullScreen = () => graphics.ToggleFullScreen();
+            Action exit = () => Exit();
+            InputManager.Update(IsActive, toggleFullScreen, exit);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, ViewPort.Camera);
-            player.Draw(spriteBatch);
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, null);
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _frameCounter.Update(deltaTime);
+            string fps = string.Format($"FPS     {_frameCounter.AverageFramesPerSecond}");
+            string loc = string.Format($"COORD {player.Coordinate.X}, {player.Coordinate.Y}");
+            string zom = string.Format($"ZOOM   {Math.Round(ViewPort.CamZoom, 1)} / {ViewPort.CamZoomDest}");
+            string lui = string.Format($"INPUT  {InputManager.InputTypeLabel[(int)InputManager.LastUsedInput]}");
+            spriteBatch.DrawString(fonts[0], fps, new Vector2(4, 1), Color.Green);
+            spriteBatch.DrawString(fonts[0], loc, new Vector2(4, 24), Color.Green);
+            spriteBatch.DrawString(fonts[0], zom, new Vector2(4, 48), Color.Green);
+            spriteBatch.DrawString(fonts[0], lui, new Vector2(4, 72), Color.Green);
             GraphicsDevice.Clear(GameOptions.BackgroundColor);
             base.Draw(gameTime);
+            spriteBatch.End();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, ViewPort.Camera);
+            player.Draw(spriteBatch);
             spriteBatch.End();
         }
     }
