e2dc352d7920a124fbad742ba99cc0e6edc67d73
Captain Cobbs <egamezhq@gmail.com>
2021-08-19 18:10:34 -0600

    Added font + dynamic font loader

---
+++ HexRPG/Fonts/Battlenet.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using SpriteFontPlus;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HexRPG.Fonts
+{
+    public  class FontLibrary
+    {
+        public static SpriteFont Battlenet(GraphicsDevice graphicsDevice)
+        {
+            TtfFontBakerResult battlenet = (TtfFontBaker.Bake(File.ReadAllBytes("./Content/Fonts/Battlenet.ttf"),
+                24,
+                1024,
+                1024,
+                new[]
+                {
+                    CharacterRange.BasicLatin
+                }
+            ));
+
+            return battlenet.CreateSpriteFont(graphicsDevice);
+                
+        }
+    }
+
+}
