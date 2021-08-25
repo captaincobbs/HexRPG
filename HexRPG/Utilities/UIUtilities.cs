7d82343eb95c9ff8c614c00b31fc7b99eb9e9dd7
AutonomyCOG <egamezhq@gmail.com>
2021-08-25 02:51:08 -0600

    Added Aligning tools given any size rectangle and the size of the object

---
+++ HexRPG/Utilities/UIUtilities.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HexRPG.Utilities
+{
+    public static class UIUtilities
+    {
+        public enum HorizontalAlignment
+        {
+            Manual,
+            Right,
+            Left,
+            Center
+        }
+
+        public enum VerticalAlignment
+        {
+            Manual,
+            Top,
+            Bottom,
+            Center
+        }
+
+        public static int HorizontalAlignRight(Rectangle renderArea, Vector2 size)
+        {
+            return renderArea.Width - (int)size.X;
+        }
+
+        public static int HorizontalAlignLeft(Rectangle renderArea, Vector2 size)
+        {
+            return 0;
+        }
+
+        public static int HorizontalAlignCenter(Rectangle renderArea, Vector2 size)
+        {
+            return (renderArea.Width - (int)size.X) / 2;
+        }
+
+        public static int VerticalAlignTop(Rectangle renderArea, Vector2 size)
+        {
+            return 0;
+        }
+
+        public static int VerticalAlignBottom(Rectangle renderArea, Vector2 size)
+        {
+            return renderArea.Height - (int)size.Y;
+        }
+
+        public static int VerticalAlignCenter(Rectangle renderArea, Vector2 size)
+        {
+            return (renderArea.Height - (int)size.Y) / 2;
+        }
+    }
+}
