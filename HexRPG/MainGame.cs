146645cbf8df26334b61d2d8edfc58978e1c43f3
AutonomyCOG <egamezhq@gmail.com>
2021-08-25 02:50:18 -0600

    Moved global command functions to maingame, moved debug overlay to its own class

--- HexRPG/MainGame.cs
+++ HexRPG/MainGame.cs
@@ -3,7 +3,6 @@ using HexRPG.Entity;
 using HexRPG.Generation;
 using HexRPG.Managers;
 using HexRPG.Sprite;
-using HexRPG.UI;
 using HexRPG.Fonts;
 using HexRPG.Utilities;
 using HexRPG.World;
@@ -12,33 +11,31 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using static HexRPG.Utilities.FileUtilities;
+using HexRPG.Debug;
+using static HexRPG.Debug.DebugOverlay;
+using System.Collections.Generic;
 
 namespace HexRPG
 {
     public class MainGame : Game
     {
         /// <summary>
-        /// Viewport for the user
-        /// </summary>
-        public ViewPort ViewPort { get; private set; }
-
-        /// <summary>
         /// Determines the format in which user files will be exported as
         /// </summary>
         public SaveType SaveType { get; set; }
 
-        Player player { get; set; }
+        public static Player Player { get; set; }
 
         // Main variables
-        public GraphicsDeviceManager graphics;
+        public static GraphicsDeviceManager Graphics;
         SpriteBatch spriteBatch;
-        MapManager mapViewer;
+        MapManager mapManager;
 
         // Art
-        SpriteFont[] fonts;
+        public static SpriteFont[] FontSet;
 
-        // Overlay
-        private FrameCounter _frameCounter = new FrameCounter();
+        // Debug
+
 
         /// <summary>
         /// Main Instance of the game that instantiates and creates all primary properties, generators, and managers.
@@ -46,32 +43,77 @@ namespace HexRPG
         public MainGame()
         {
             // Create main game variables
-            graphics = new GraphicsDeviceManager(this);
+            Graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            SaveType = SaveType.JSON;
-            player = new Player("");
-            ViewPort = new ViewPort(Window.ClientBounds.Height, Window.ClientBounds.Width, CameraFocus.Player,player);
+            SaveType = GameOptions.SaveType;
+            Player = new Player("");
 
             // Window properties
             IsMouseVisible = true;
-            graphics.HardwareModeSwitch = false;
-            graphics.PreferredBackBufferHeight = Window.ClientBounds.Height;
-            graphics.PreferredBackBufferWidth = Window.ClientBounds.Width;
+            Graphics.HardwareModeSwitch = false;
+            Graphics.PreferredBackBufferHeight = Window.ClientBounds.Height;
+            Graphics.PreferredBackBufferWidth = Window.ClientBounds.Width;
             Window.AllowUserResizing = true;
-            graphics.ApplyChanges();
+            Graphics.ApplyChanges();
+
+            // Debugging
+            DebugOverlay.OverlayContents = new List<DebugOverlayItemType>()
+            {
+            DebugOverlayItemType.FPS,
+            DebugOverlayItemType.Zoom,
+            DebugOverlayItemType.Coordinates,
+            DebugOverlayItemType.LastInputDevice,
+            DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+DebugOverlayItemType.FPS,
+
+            };
         }
 
         protected override void Initialize()
         {
             base.Initialize();
             InputManager.Initialize();
+            DebugOverlay.Initialize();
+            ViewPort.Initialize(Window.ClientBounds.Height, Window.ClientBounds.Width, CameraFocus.Player, Player);
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             TextureIndex.LoadContent(Content);
-            fonts = new SpriteFont[]
+            FontSet = new SpriteFont[]
             {
                 FontLibrary.Battlenet(GraphicsDevice)
             };
@@ -80,15 +122,16 @@ namespace HexRPG
 
         protected override void Update(GameTime gameTime)
         {
-            player.Update();
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Secondary Updates
+            Player.Update();
             ViewPort.Update(Window);
-            // Camera scrolling each frame - subtracting (GameOptions.TileSize/2) centers the screen
 
 
             // Main Updates
-            Action toggleFullScreen = () => graphics.ToggleFullScreen();
-            Action exit = () => Exit();
-            InputManager.Update(IsActive, toggleFullScreen, exit);
+            InputManager.Update(IsActive, Exit);
+            DebugOverlay.Update(deltaTime);
 
             base.Update(gameTime);
         }
@@ -96,22 +139,38 @@ namespace HexRPG
         protected override void Draw(GameTime gameTime)
         {
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, null);
-            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            _frameCounter.Update(deltaTime);
-            string fps = string.Format($"FPS     {_frameCounter.AverageFramesPerSecond}");
-            string loc = string.Format($"COORD {player.Coordinate.X}, {player.Coordinate.Y}");
-            string zom = string.Format($"ZOOM   {Math.Round(ViewPort.CamZoom, 1):0.0} / {ViewPort.CamZoomDest:0.0}");
-            string lui = string.Format($"INPUT  {InputManager.InputTypeLabel[(int)InputManager.LastUsedInput]}");
-            spriteBatch.DrawString(fonts[0], fps, new Vector2(4, 0), Color.Green);
-            spriteBatch.DrawString(fonts[0], loc, new Vector2(4, 24), Color.Green);
-            spriteBatch.DrawString(fonts[0], zom, new Vector2(4, 48), Color.Green);
-            spriteBatch.DrawString(fonts[0], lui, new Vector2(4, 72), Color.Green);
             GraphicsDevice.Clear(GameOptions.BackgroundColor);
+            DebugOverlay.Draw(spriteBatch);
             base.Draw(gameTime);
             spriteBatch.End();
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, ViewPort.Camera);
-            player.Draw(spriteBatch);
+            Player.Draw(spriteBatch);
             spriteBatch.End();
+        }
+
+        public static void ToggleFullscreen()
+        {
+            Graphics.ToggleFullScreen();
+            ViewPort.WindowHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            ViewPort.WindowWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            UpdateForNewScreenSize();
+        }
+
+        public void ExitGame()
+        {
+            Exit();
+        }
+
+        public static void DebugToggle()
+        {
+            IsVisible = !IsVisible;
+        }
+
+        public void WindowResized()
+        {
+            ViewPort.WindowHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            ViewPort.WindowWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            UpdateForNewScreenSize();
         }
     }
 }
