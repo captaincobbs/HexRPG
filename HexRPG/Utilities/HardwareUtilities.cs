bfefe67f53476a0ff074084b3d75e9f3a50ac977
AutonomyCOG <egamezhq@gmail.com>
2021-08-25 02:50:46 -0600

    Added way to read GraphicsDevice

---
+++ HexRPG/Utilities/HardwareUtilities.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework;
+
+namespace HexRPG.Utilities
+{
+    public static class HardwareUtilities
+    {
+        public static GraphicsDeviceManager GraphicsDevice { get; set; }
+        public static AudioEngine AudioDevice { get; set; }
+        public static string GetVideoDeviceName()
+        {
+            return GraphicsDevice.GraphicsDevice.Adapter.DeviceName;
+        }
+
+        public static string GetAudioDeviceName()
+        {
+            return "";
+        }
+    }
+}
