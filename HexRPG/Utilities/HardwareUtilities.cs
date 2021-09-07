fb3acae3f19bc7da249c416b74ad3f1a1eca81dc
Captain Cobbs <egamezhq@gmail.com>
2021-09-07 00:15:46 -0600

    Cached management names so that it wasn't searching the registry each frame

--- HexRPG/Utilities/HardwareUtilities.cs
+++ HexRPG/Utilities/HardwareUtilities.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework;
+using System.Management;
+using System.Collections.Generic;
+using System;
 
 namespace HexRPG.Utilities
 {
@@ -7,9 +10,27 @@ namespace HexRPG.Utilities
     {
         public static GraphicsDeviceManager GraphicsDevice { get; set; }
         public static AudioEngine AudioDevice { get; set; }
+
+        private static Dictionary<string, string> systemValues {get; set;}
+
+        public static void Initialize()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            systemValues = new Dictionary<string, string>();
+
+            systemValues.Add("GPUName", GraphicsDevice.GraphicsDevice.Adapter.Description);
+
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                systemValues.Add("ProcessorName", (string)queryObj["Name"]);
+            }
+
+            systemValues.Add("AssemblyRuntimeVersion", typeof(string).Assembly.ImageRuntimeVersion);
+        }
+
         public static string GetVideoDeviceName()
         {
-            return GraphicsDevice.GraphicsDevice.Adapter.Description;
+            return systemValues["GPUName"];
         }
 
         public static string GetAudioDeviceName()
@@ -19,7 +40,12 @@ namespace HexRPG.Utilities
 
         public static string GetChipSetName()
         {
-            return System.Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+            return systemValues["ProcessorName"];
+        }
+
+        public static string GetAssemblyRuntimeVersion()
+        {
+            return systemValues["AssemblyRuntimeVersion"];
         }
     }
 }
