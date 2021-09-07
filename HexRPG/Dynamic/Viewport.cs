b4da41d5072270cbc024713d51b0cf3e4027c2d9
AutonomyCOG <egamezhq@gmail.com>
2021-09-06 21:39:41 -0600

    Documentation

--- HexRPG/Dynamic/Viewport.cs
+++ HexRPG/Dynamic/Viewport.cs
@@ -5,13 +5,14 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using HexRPG.Overlay;
 
 namespace HexRPG.Dynamic
 {
     public static class ViewPort
     {
         public static Matrix Camera { get; private set; }
-        public static CameraFocus cameraFocus { get; set; }
+        public static IFocusObject CameraFocus { get; set; }
 
         // Camera properties
         /// <summary>
@@ -35,15 +36,20 @@ namespace HexRPG.Dynamic
         static float verticalBounds = 0f;
         static float cameraSmoothing = 10;
 
-        public static void Initialize(CameraFocus cameraFocus)
+        public static void Initialize(IFocusObject cameraFocus)
         {
-            ViewPort.cameraFocus = cameraFocus;
+            ViewPort.CameraFocus = cameraFocus;
         }
 
-        public static void Update(GameWindow window)
+        public static void Update()
         {
             // Apply zoom delta to current zoom value
             ZoomValue += ((ZoomDestination - ZoomValue) * GameOptions.CameraZoomInertia);
+
+            if (CameraFocus.GetType() != typeof(PlayerControlled))
+            {
+                Coordinates = Vector2.Lerp(Coordinates, CameraFocus.GetPosition(), 1f);
+            }
 
             // Scroll In
             if (InputManager.IsActionPressed(InputManager.InputAction.ZoomIn))
@@ -65,31 +71,19 @@ namespace HexRPG.Dynamic
 
             // Keep camera zoom within a specific range
             ZoomDestination = MathUtilities.Clamp(ZoomDestination, minCamZoom, maxCamZoom);
-            ZoomValue = MathUtilities.Clamp(ZoomValue, minCamZoom, maxCamZoom);
 
             // Change camera matrix properties with updated information
             Camera =
-                Matrix.CreateTranslation(new Vector3(Coordinates, 0)) *
+                Matrix.CreateTranslation(new Vector3(Coordinates.X, Coordinates.Y, 0)) *
                 Matrix.CreateScale(new Vector3(ZoomValue, ZoomValue, 1)) *
                 Matrix.CreateTranslation(new Vector3(MainGame.GameWindow.ClientBounds.Width / 2,
                 MainGame.GameWindow.ClientBounds.Height / 2, 0));
         }
-
-        public static void Follow()
-        {
-
-        }
-    }
-    
-    public class CameraFocus
-    {
-        public IFocusObject FocusObject { get; set; }
-        public CameraFocus(IFocusObject focusObject)
-        {
-            FocusObject = focusObject;
-        }
     }
 
+    /// <summary>
+    /// Object to store information about the current focus of the ca
+    /// </summary>
     public interface IFocusObject
     {
         public Vector2 GetPosition()
@@ -98,33 +92,68 @@ namespace HexRPG.Dynamic
         }
     }
 
+    /// <summary>
+    /// <see cref="IFocusObject"/> for tracking an input entity with the camera
+    /// </summary>
     public class EntityFocus : IFocusObject
     {
         private IEntity focusObject { get; set; }
 
+        /// <summary>
+        /// <see cref="IFocusObject"/> for tracking an input entity with the camera
+        /// </summary>
+        /// <param name="focusObject"><see cref="IEntity"/> to track</param>
         public EntityFocus(IEntity focusObject)
         {
             this.focusObject = focusObject;
         }
 
+        /// <summary>
+        /// Returns current position of the tracked entity in the <see cref="IFocusObject"/>
+        /// </summary>
+        /// <returns><see cref="Vector2"/> of the current position of the tracked entity</returns>
         public Vector2 GetPosition()
         {
             return focusObject.AnimCoordinate;
         }
     }
 
+    /// <summary>
+    /// <see cref="IFocusObject"/> for storing a static coordinate as the camera's target position
+    /// </summary>
     public class StaticFocus : IFocusObject
     {
         private Vector2 staticPosition { get; set; }
 
+        /// <summary>
+        /// <see cref="IFocusObject"/> for storing a static coordinate as the camera's target position
+        /// </summary>
+        /// <param name="staticPosition"><see cref="Vector2"/> coordinates to focus the camera on</param>
         public StaticFocus(Vector2 staticPosition)
         {
             this.staticPosition = staticPosition;
         }
 
+        /// <summary>
+        /// Returns the static position stored in the <see cref="IFocusObject"/>
+        /// </summary>
+        /// <returns><see cref="Vector2"/> of Static Coordinate</returns>
         public Vector2 GetPosition()
         {
             return staticPosition;
+        }
+    }
+
+    public class PlayerControlled : IFocusObject
+    {
+        public PlayerControlled()
+        {
+            throw new NotImplementedException("PlayerControlled : IFocusObject");
+        }
+
+        public Vector2 GetPosition()
+        {
+            return new Vector2(0, 0);
         }
     }
 }
