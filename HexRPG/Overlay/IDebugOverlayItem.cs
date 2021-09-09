2aa8904912b6504bc4ac26d6cfcfd2a2252b6ff6
AutonomyCOG <egamezhq@gmail.com>
2021-09-08 22:42:40 -0600

    Added Offset to IDebugOverlayItem

--- HexRPG/Overlay/IDebugOverlayItem.cs
+++ HexRPG/Overlay/IDebugOverlayItem.cs
@@ -9,16 +9,44 @@ namespace HexRPG.Overlay
 {
     public interface IDebugOverlayItem
     {
+        /// <summary>
+        /// The current location, in pixels, of the <see cref="IDebugOverlayItem"/> on the screen
+        /// </summary>
         public Vector2 Coordinates { get; set; }
 
+        /// <summary>
+        /// Offset added to coordinates after alignment is applied
+        /// </summary>
+        public Vector2 Offset { get; set; }
+
+        /// <summary>
+        /// Determines how the <see cref="IDebugOverlayItem"/> is aligned horiziontally
+        /// </summary>
         public HorizontalAlignment horizontalAlignment { get; set; }
 
+        /// <summary>
+        /// Determines how the <see cref="IDebugOverlayItem"/> is aligned vertically
+        /// </summary>
         public VerticalAlignment verticalAlignment { get; set; }
 
+        /// <summary>
+        /// Runs logic the <see cref="IDebugOverlayItem"/> needs each frame
+        /// </summary>
+        /// <param name="gameTime">Current time state of the <see cref="MainGame"/></param>
         public void Update(GameTime gameTime) { }
 
+        /// <summary>
+        /// Allows the <see cref="IDebugOverlayItem"/> to render itself
+        /// </summary>
+        /// <param name="spriteBatch">Most recent <see cref="SpriteBatch"/></param>
+        /// <param name="font">The <see cref="SpriteFont"/> the <see cref="IDebugOverlayItem"/> is rendered with</param>
+        /// <param name="color">The color that the <see cref="SpriteFont"/> is rendered with</param>
         public void Draw(SpriteBatch spriteBatch, SpriteFont font, Color color) { }
 
+        /// <summary>
+        /// Re-aligns <see cref="IDebugOverlayItem"/> after a change in the screen size
+        /// </summary>
+        /// <param name="font">The <see cref="SpriteFont"/> used in the <see cref="IDebugOverlayItem"/></param>
         public void RecalculatePosition(SpriteFont font) { }
     }
 }
