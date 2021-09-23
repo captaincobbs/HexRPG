290bd4632e1ba6d09a5d7f04a0689caf6d71556e
Captain Cobbs <egamezhq@gmail.com>
2021-09-22 18:59:02 -0600

    Made the world generator not crash

--- HexRPG/Generation/MapGenerator.cs
+++ HexRPG/Generation/MapGenerator.cs
@@ -1,4 +1,6 @@
-﻿using HexRPG.World;
+﻿using HexRPG.Utilities;
+using HexRPG.World;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +13,17 @@ namespace HexRPG.Generation
     /// </summary>
     public static class MapGenerator
     {
+        public static Tile[,] Generate(Vector2 size, int side, int seed)
+        {
+            float h = MathUtilities.CalculateH(side);
+            float r = MathUtilities.CalculateR(side);
 
+            Vector2 PixelSize = new Vector2(size.X * Globals.TileSize, size.Y * Globals.TileSize);
+            Vector2 hexSize = new Vector2(side + h, r + r);
+            PixelSize = new Vector2((size.X * hexSize.X) + h, (size.Y * hexSize.Y) + r);
+
+
+            return new Tile[(int)size.Y, (int)size.X];
+        }
     }
 }
