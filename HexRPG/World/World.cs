50f8da6971b2996f024a9955ba36cb81b5ba4a08
Captain Cobbs <egamezhq@gmail.com>
2021-09-22 18:59:59 -0600

    Renamed Map to World, added necessary functions

---
+++ HexRPG/World/World.cs
@@ -0,0 +1,33 @@
+﻿using HexRPG.Generation;
+using HexRPG.Utilities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using static HexRPG.Utilities.FileUtilities;
+
+namespace HexRPG.World
+{
+    public class World
+    {
+        public Chunk[,] Chunks { get; set; }
+        public int worldSize { get; set; }
+        public List<Chunk> dirtyChunks { get; set; }
+
+        public World()
+        {
+
+        }
+
+        public Tile GetTile(Vector2 coordinates, bool forceLoad)
+        {
+            return null;
+        }
+
+        public Chunk GetChunk(Vector2 coordinates, bool forceLoad)
+        {
+            return null;
+        }
+    }
+}
