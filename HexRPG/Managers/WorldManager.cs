316a562fb0be388e847b0bab7232e519e5db0e6d
Captain Cobbs <egamezhq@gmail.com>
2021-09-22 18:59:21 -0600

    Renamed MapManager to WorldManager, added ChunkUpdate and ChunkDraw range functions

---
+++ HexRPG/Managers/WorldManager.cs
@@ -0,0 +1,46 @@
+﻿using HexRPG.Entity;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// TODO - Fast way to do tile lookups without needing cos/sin to avoid lag
+
+namespace HexRPG.Managers
+{
+    public static class WorldManager
+    {
+        public static World.World World { get; set; }
+
+        public static void Initialize()
+        {
+
+        }
+
+        public static void UpdateChunkRange(GameTime gameTime, Rectangle area)
+        {
+            
+        }
+
+        public static void DrawChunkRange(GameTime gameTime, Rectangle area)
+        {
+
+        }
+
+        public static void Save()
+        {
+
+        }
+
+        public static World.World Load()
+        {
+            return null;
+        }
+
+        public static Rectangle GetAreaFromPlayer(Player player)
+        {
+            return new Rectangle((int)player.Coordinates.X - Globals.ChunkLimit, (int)player.Coordinates.Y - Globals.ChunkLimit, (int)player.Coordinates.X + Globals.ChunkLimit, (int)player.Coordinates.Y + Globals.ChunkLimit);
+        }
+    }
+}
