0eada4969b8c697193c151a3c391b6a29afe331a
Captain Cobbs <egamezhq@gmail.com>
2021-10-06 18:57:50 -0600

    Added BiomeType

---
+++ HexRPG/World/Biome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HexRPG.World
+{
+    public static class Biome
+    {
+        public static BiomeType GetBiomeFromNoise(float noise)
+        {
+            return BiomeType.Ocean;
+        }
+
+        public enum BiomeType
+        {
+            Ocean =  0,
+            Plains = 1,
+            Desert = 2,
+        }
+    }
+}
