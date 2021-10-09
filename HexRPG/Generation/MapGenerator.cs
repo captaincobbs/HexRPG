3a12bf497936850b5e65d69966e912cf3efb7ff9
AutonomyCOG <egamezhq@gmail.com>
2021-10-09 01:15:54 -0600

    Added different generator presets

--- HexRPG/Generation/MapGenerator.cs
+++ HexRPG/Generation/MapGenerator.cs
@@ -15,12 +15,62 @@ namespace HexRPG.Generation
     /// </summary>
     public static class MapGenerator
     {
-        public static GeneratorUtilities RNG = new GeneratorUtilities();
+        public static NoiseGenerator ElevationGenerator = new NoiseGenerator();
+        public static NoiseGenerator BiomeGenerator = new NoiseGenerator();
+        public static NoiseGenerator WarpBiomeGenerator = new NoiseGenerator();
+        public static NoiseGenerator FertilityGenerator = new NoiseGenerator();
+        public static NoiseGenerator RichnessGenerator = new NoiseGenerator();
+        public static NoiseGenerator DangerGenerator = new NoiseGenerator();
+
+        /// <summary>
+        /// Initializes the <see cref="MapGenerator"/>, and prepares the various types of noise generators
+        /// </summary>
+        public static void Initialize()
+        {
+            // Biome Generator
+            BiomeGenerator.SetNoiseType(NoiseGenerator.NoiseType.Cellular);
+            BiomeGenerator.SetFrequency(0.015f);
+            BiomeGenerator.SetCellularDistanceFunction(NoiseGenerator.CellularDistanceFunction.Hybrid);
+            BiomeGenerator.SetCellularReturnType(NoiseGenerator.CellularReturnType.CellValue);
+
+            // Warp
+            WarpBiomeGenerator.SetDomainWarpType(NoiseGenerator.DomainWarpType.OpenSimplex2);
+            WarpBiomeGenerator.SetDomainWarpAmp(100f);
+            WarpBiomeGenerator.SetFrequency(0.010f);
+            WarpBiomeGenerator.SetFractalType(NoiseGenerator.FractalType.DomainWarpIndependent);
+            WarpBiomeGenerator.SetFractalLacunarity(1.5f);
+
+            // Fertility
+            FertilityGenerator.SetNoiseType(NoiseGenerator.NoiseType.OpenSimplex2);
+            FertilityGenerator.SetFrequency(0.005f);
+            FertilityGenerator.SetFractalType(NoiseGenerator.FractalType.FBm);
+            FertilityGenerator.SetFractalOctaves(2);
+            FertilityGenerator.SetFractalLacunarity(-2.0f);
+
+            // Richness
+
+
+            // Danger
+            DangerGenerator.SetNoiseType(NoiseGenerator.NoiseType.Perlin);
+            DangerGenerator.SetFrequency(0.06f);
+            DangerGenerator.SetFractalType(NoiseGenerator.FractalType.FBm);
+            DangerGenerator.SetFractalGain(1.0f);
+            DangerGenerator.SetFractalWeightedStrength(0.5f);
+        }
+
+        /// <summary>
+        /// Changes the seeds of all generators to input integer
+        /// </summary>
+        /// <param name="seed">Seed to set all generators to</param>
+        private static void SetSeed(int seed)
+        {
+            BiomeGenerator.SetSeed(seed);
+            WarpBiomeGenerator.SetSeed(seed);
+        }
 
         public static Tile[,] Generate(Vector2 size, Vector2 chunkCoordinates, int seed)
         {
-            RNG.SetNoiseType(GeneratorUtilities.NoiseType.OpenSimplex2);
-            RNG.SetSeed(seed);
+            SetSeed(seed);
             Tile[,] Tiles = new Tile[(int)size.X, (int)size.Y];
 
             for (int x = 0; x < size.X; x++)
@@ -31,6 +81,7 @@ namespace HexRPG.Generation
                     Tiles[x, y].ChunkCoordinates = chunkCoordinates;
                     Tiles[x, y].Coordinates = position;
                     Tiles[x, y].Biome = GenerateBiome(position);
+                    Tiles[x, y].Element = GenerateElement(position);
                 }
             }
 
@@ -39,7 +90,24 @@ namespace HexRPG.Generation
 
         public static BiomeType GenerateBiome(Vector2 position)
         {
+            float xf = position.X;
+            float yf = position.Y;
+
+            WarpBiomeGenerator.DomainWarp(ref xf, ref yf);
+            float noise = BiomeGenerator.GetNoise(xf, yf);
+
             return BiomeType.Plains;
+        }
+
+        public static Element GenerateElement(Vector2 position)
+        {
+            float xf = position.X;
+            float yf = position.Y;
+
+            WarpBiomeGenerator.DomainWarp(ref xf, ref yf);
+            float noise = 0 - BiomeGenerator.GetNoise(xf, yf);
+
+            return Element.Neutral;
         }
     }
 }
